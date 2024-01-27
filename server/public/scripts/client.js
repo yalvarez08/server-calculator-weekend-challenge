e737c2c982a6bc5ff38b363389a5de247fe19426
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-01-27 11:45:57 -0500

    created result history function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,14 +9,46 @@ function onReady() {
     event.preventDefault();
 
     let userInputs = {
-        input1 = document.getElementById("input_1").value,
-        input2 = document.getElementById("input_2").value
+        input1: document.getElementById("input_1").value,
+        input2: document.getElementById("input_2").value
 
     };
-    axios({
+    axios({  //make POST request to send to server
         method: "POST",
         url: "/calculations",
         data: userInputs,
       })
-        .then
+        .then (function(response) {
+            console.log('request to POST /calculations succeeded.', response.status);
+            document.getElementById("input_1").value = "";
+            document.getElementById("input_2").value = "";
+            resultHistory();
+        })
+        .catch(function (error) {
+            alert('Request to POST /calculations failed.');
+            console.log("error", error);
+          });
+  }
+
+  function resultHistory() {
+    axios({
+        method: "GET",
+        url: "/calculations",
+    })
+      .then (function(response) {
+        console.log('request to GET /calculations succeeded.', response.status);
+
+
+
+
+
+
+    //make history reflect on DOM
+    document.getElementById("resultHistory").innerHTML 
+
+      })
+      .catch(function (error) {
+        alert('Request to GET /calculations failed.');
+        console.log("error", error);
+      });
   }
